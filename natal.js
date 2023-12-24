42ffac061bb223bc13ea1b42e545ee7966797e44
FellypeGC <gabrielfellypesteve@gmail.com>
2023-12-24 02:32:17 -0300

    commit

--- natal.js
+++ natal.js
@@ -1,7 +1,15 @@
 function openGift() {
   let presente = document.getElementById('presente');
+  
+  if (presente.style.visibility === 'hidden') {
+    presente.style.visibility = 'visible';
+  } else {
+    presente.style.visibility = 'hidden';
+  }
+};
 
-}
-
-
+document.addEventListener('DOMContentLoaded', function () {
+  let btn = document.getElementById('btn');
+  btn.addEventListener('click', openGift);
+});
 
